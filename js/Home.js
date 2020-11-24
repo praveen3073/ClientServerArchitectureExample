de833048cd1ef088498db65bea38d4f004e0768d
praveen3073 <pop3073@gmail.com>
2020-11-24 16:40:16 +0530

    Added: Create a simple html file to test the REST services with JSON Server using Promise for Employee Payroll Service

---
+++ js/Home.js
@@ -0,0 +1,74 @@
+let empPayrollList;
+window.addEventListener('DOMContentLoaded',(event)=>{ 
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector('.emp-count').textContent = empPayrollList.length;
+    createInnerHTML();
+    localStorage.removeItem('editEmp');
+});
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem("EmployeePayrollList") ?
+        JSON.parse(localStorage.getItem("EmployeePayrollList")) :
+        [];
+}
+
+function createInnerHTML(){
+    if(empPayrollList.length == 0) return;
+    const headerHTML=
+        "<th></th>"+
+        "<th>Emp Name</th>"+
+        "<th>Gender</th>"+
+        "<th>Department</th>"+
+        "<th>Salary</th>"+
+        "<th>Start Date</th>"+
+        "<th>Actions</th>";
+    
+    let innerHTML = `${headerHTML}`;    
+    for(const empData of empPayrollList) {
+        innerHTML = `${innerHTML}
+        <tr>
+            <td><img class="profile" src="${empData._profilePic}" alt="Profile Pic"></td>
+            <td>${empData._name}</td>
+            <td>${empData._gender}</td>
+            <td>${getDeptHTML(empData._department)}</td>
+            <td>RS ${empData._salary}</td>
+            <td>${stringifyDate(empData._startDate)}</td>
+            <td>
+                <img id="${empData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img id="${empData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+            </td>
+        </tr>
+        `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHTML;
+}
+
+function getDeptHTML(deptList) {
+    let deptHTML = '';
+    for(const dept of deptList) {
+        deptHTML = `${deptHTML}
+        <div class="dept-label">${dept}</div>`;
+    }
+    return deptHTML;
+}
+
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+    if(!empPayrollData) return;
+    localStorage.setItem('editEmp', JSON.stringify(empPayrollData));
+    window.location.replace(site_properties.add_emp_payroll_page);
+}
+
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+    if(!empPayrollData) { alert("Not deleted"); return;}
+    const index = empPayrollList
+                    .map(empData => empData._id)
+                    .indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector('.emp-count').textContent = empPayrollList.length;
+    createInnerHTML();
+    alert("Deleted");
+}
+
