055f14a854b310b1a3f52bf54d55fb4fc10d910f
praveen3073 <pop3073@gmail.com>
2020-11-24 19:07:52 +0530

    Added: POST And PUT In Employee Payroll Service Using Json Server

--- js/PayrollForm.js
+++ js/PayrollForm.js
@@ -102,13 +102,34 @@ const save = (event) => {
     event.stopPropagation();
     try{
         setEmployeePayrollObject();
-        createAndUpdateStorage();
-        resetForm();
-        window.location.replace(site_properties.home_page);
+        if(site_properties.use_local_storage.match("true")) {
+            createAndUpdateStorage();
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        } else {
+            createOrUpdateEmployeePayroll();
+        }
     }catch(exception){
         console.error(exception);
         return;
     }
+}
+
+const createOrUpdateEmployeePayroll = () => {
+    let postURL = site_properties.server_url;
+    let methodCall = "POST";
+    if(isUpdate) {
+        methodCall = "PUT";
+        postURL = postURL + "/" + employeePayrollObj.id.toString(); 
+    }
+    makeServiceCall(methodCall, postURL, true, employeePayrollObj)
+        .then(responseText => {
+            resetForm();
+            window.location.replace(site_properties.home_page);
+        })
+        .catch(error => {
+            throw error;
+        });
 }
 
 function setEmployeePayrollObject() {
